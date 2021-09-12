82b34fff13c1e381b1739491ba4107bc49ed4e30
JBermudezG <jebermudez587@gmail.com>
2021-09-11 20:28:12 -0500

    Comments form control

--- src/components/CommentsForm.js
+++ src/components/CommentsForm.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles/CommentsForm.scss';
 
 export default function CommentsForm(){
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [comment, setComment] = useState("");
+
+	const handleChange = e => {
+		//console.log(e);
+		if(e.target.name === "name"){
+			setName(e.target.value);
+		}else if(e.target.name === "email"){
+			setEmail(e.target.value);
+		}else if(e.target.name === "comment"){
+			setComment(e.target.value);
+		}
+	}
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("The form has been submited!");
+	}
 	return(
 		<>
 			<h2>COMMENTS</h2>
 			<p>Add comments:</p>
-			<form>
-				<input type="text" placeholder="Name"/>
-				<input type="mail" placeholder="Email"/>
-				<textarea placeholder="Write your comment here"></textarea>
+			<form onSubmit={handleSubmit}>
+				<input type="text" placeholder="Name" name="name" onChange={handleChange} value={name}/>
+				<input type="mail" placeholder="Email" name="email" onChange={handleChange} value={email}/>
+				<textarea placeholder="Write your comment here" name="comment" onChange={handleChange} value={comment}></textarea>
 				<input type="submit" value="SUBMIT"/>
 			</form>
 		</>
